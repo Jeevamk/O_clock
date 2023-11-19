3d849e4852afb020d5c4c97de17dd536e9d9cbf0
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2023-11-19 22:33:24 +0530

    wishlist

--- assets/js/wish.js
+++ assets/js/wish.js
@@ -14,17 +14,76 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             body: JSON.stringify(wishProduct)
           })
-          .then(response => {
-            if (response.ok) {
-              addWishList.style.color = 'red'; 
-            } else {
-              console.error('Error adding product to wishlist:', response.statusText);
-            }
-          })
-          .catch(error => {
-            console.error('Fetch error:', error);
-          });
+        //   .then(response => {
+        //     if (response.ok) {
+        //         alert("Product add Succefully")
+        //     } else {
+        //       console.error('Error adding product to wishlist:', response.statusText);
+        //     }
+        //   })
+        //   .catch(error => {
+        //     console.error('Fetch error:', error);
+        //   });
         });
     }
   
     });
+
+
+//delete product from wishlist//
+const deletebutton = document.querySelectorAll(".deleteWishproduct");
+deletebutton.forEach((btn) => {
+  btn.addEventListener("click", async (event) => {
+    const Idproduct = await event.target.getAttribute("data-user-id");
+
+    try {
+      const response = await fetch(`/wish/delete/${Idproduct}`);
+      if (response.ok) {
+        const productData = await response.json();
+        document.getElementById(
+          "wishDelete"
+        ).innerHTML = `<div id="deletealert"><h5> Are you confirm to delete this product from wishlist </h5> </div>
+        <form id="deleteProductData">
+        <input type="text" class="form-control" hidden value="${productData._id}" name="id">
+        <div class="col-sm-10">
+        <button type="button" class="btn btn-dark"  onclick="deleteWish()" id="deleteButton">yes</button>
+      </div>
+      </form>
+        `;
+        const showModal = new bootstrap.Modal(
+          document.getElementById("deleteWish")
+        );
+        showModal.show();
+      } else {
+        console.error("Error fetching user data:", error);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  });
+});
+
+
+
+function deleteWish() {
+    const DeleteData = document.getElementById("deleteProductData");
+    const myDeleteData = new FormData(DeleteData);
+  
+    fetch(`/wish/delete`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(Object.fromEntries(myDeleteData)),
+    })
+      .then((response) => {
+        if (response.ok) {
+          window.location.href = "/wish";
+        }
+        throw new Error("not ok");
+      })
+  
+      .catch((error) => {
+        console.log(error);
+      });
+  }
--- server/routes/user/wishlist.js
+++ server/routes/user/wishlist.js
@@ -6,39 +6,39 @@ const productCollection = require("../../model/product_model")
 const wishcollection= require("../../model/wish_model")
 
 
-route.get('/',logauth,async(req,res) =>{
-    // const userId = req.userId;
-    // const wishProduct = await wishcollection.find({userId});
+route.get('/', logauth, async (req, res) => {
+    try {
+        const userId = req.userId;
+        const wishlistProducts = await wishcollection.find({ userId });
 
-    res.render('wishlist')
-})
+        const wishProducts = [];
 
-// route.post('/',logauth, async (req, res) => {
-//     try {
-//       const { productId } = req.body;
-//       const userId = req.userId; 
-      
-//       const WishProduct = new wishcollection ({ userId, productId });
-//       const WishSave = await WishProduct.save()
-  
-//       if (WishSave) {
-//         const _id = WishSave.productId;
-//         const product = await productCollection.findById(_id)
-//         res.render("productDetails",{product})
-//       }else {
-//         return res.status(400).json({ message: 'Product do not added to wishlist successfully' });
-//       }
+        for (let newWish of wishlistProducts){
+            const productId = newWish.productId
 
-//     } catch (error) {
-//       console.error(error);
-//       return res.status(500).json({ message: 'Internal server error' });
-//     }
-//   });
+            const productContent = await productCollection.find({_id:productId})
 
-route.post('/', logauth, async (req, res) => {
+            if(productContent){
+                wishProducts.push({productContent,quantity:1})
+            }
+        }
+        console.log(wishProducts);
+        res.render('wish',{ wishProducts });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+
+//add product to the wishlist//
+route.post('/', logauth , async (req, res) => {
     try {
         const { productId } = req.body;
         const userId = req.userId;
+        // if (!userId){
+        //     return res.render("user_login");
+        // }
 
         const existingWish = await wishcollection.findOne({ userId, productId });
 
@@ -64,5 +64,32 @@ route.post('/', logauth, async (req, res) => {
     }
 });
 
+//delete the product//
+route.get("/delete/:id",logauth, async (req, res) => {
+    
+      const Idproduct = req.params._id;
+  
+      try {
+        const product = await wishcollection.findOne({ Idproduct });
+        if (product) {
+          res.json(product);
+        } else {
+          res.status(404).json({ error: "product not found" });
+        }
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "internal server error" });
+      }
+    
+  });
 
+  route.delete("/delete", logauth, async (req, res) => {
+    try {
+        const proId = req.body.id;
+        await wishcollection.findByIdAndDelete(proId);
+        res.status(204).send(); // 204 No Content: The server successfully processed the request, but there is no content to send.
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
 module.exports= route;
