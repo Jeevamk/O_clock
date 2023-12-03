ae2fe9ca51f61573a65cf31e07149da0f36ef0b0
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2023-12-03 16:00:37 +0530

    checkout

--- server/routes/user/checkout.js
+++ server/routes/user/checkout.js
@@ -7,13 +7,17 @@ const cartcollection = require('../../model/cart_model');
 const productCollection = require("../../model/product_model");
 
 
-
+route.get('/',async(req,res) =>{
+  res.render("checkout")
+})
 
 route.post('/',logauth, async(req,res) =>{
  
   try {
     const userId = req.userId;
     const user = await userCollection.findById(userId)
+    const checkoutAddress = await checkoutCollection.find({userId})
+    console.log("jdhfjhf",checkoutAddress);
 
     console.log(userId);
     const cartProducts = await cartcollection.find({ userId });
@@ -28,7 +32,7 @@ route.post('/',logauth, async(req,res) =>{
     const grandTotal = req.body.checkoutvalue;
    
     console.log(cartItems);
-    res.render("checkout", { cartItems , user , grandTotal});
+    res.render("checkout", { cartItems , user , grandTotal , checkoutAddress});
 } 
 catch (error) {
     res.status(500).send("Internal Server Error");
@@ -40,18 +44,28 @@ catch (error) {
 route.post('/address',logauth, async(req,res) => {
   try {
     const userId = req.userId;
-    const { name , phone , address , area , pincode , city , state , optionaladdress } = req.body;
-    
+    const { name , phone , address , area , pincode , city , state , optionaladdress ,addressId ,grandTotal } = req.body;
+    console.log('User ID:', userId);  
+    console.log('Address Data:', { name, phone, address, area, pincode, city, state, optionaladdress, addressId });
 
     
 
     const newCheckout = new checkoutCollection ({
       userId, name , phone , address , area , pincode , city , state , optionaladdress
     })
-
     await newCheckout.save();
 
-    res.redirect("/checkout")
+    const cartProducts = await cartcollection.find({ userId });
+    const cartItems = await Promise.all(cartProducts.map(async (newcart) => {
+        const productId = newcart.productId;
+        const quantity = newcart.quantity;
+
+        const cartContent = await productCollection.find({ _id: productId });
+        return cartContent ? { cartContent, quantity } : null;
+       
+    }));
+
+    res.render("checkout",{cartItems ,newCheckout ,grandTotal})
   } catch (error) {
     res.status(500).send('Internal Server Error');
 }
