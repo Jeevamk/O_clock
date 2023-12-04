3a50a08c4e3aa482063472e805fa78c161c45464
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2023-12-04 23:16:13 +0530

    checkout-delete

--- assets/js/checkoutOrder.js
+++ assets/js/checkoutOrder.js
@@ -12,3 +12,64 @@ function editAddress(addressId,name,phone,email,address,area,pincode,city,state,
     
 
 }
+
+
+//delete//
+
+document.querySelectorAll(".deleteAddress").forEach((btn) => {
+    btn.addEventListener("click", async (event) => {
+      const IdAddress = await event.target.getAttribute("data-address-id");
+  
+      try {
+        const response = await fetch(`/checkout/delete/${IdAddress}`);
+        if (response.ok) {
+          const addressData = await response.json();
+          document.getElementById(
+            "addressDelete"
+          ).innerHTML = `<div id="deletealert"><h5> Are you confirm to delete the Address </h5> </div>
+                <form id="deleteAddressData">
+                <input type="text" class="form-control" hidden value="${addressData._id}" name="id">
+                <div class="col-sm-10">
+                <button type="button" class="btn btn-dark"  onclick="deleteAddress()" id="deleteButton">yes</button>
+              </div>
+              </form>
+                `;
+  
+          // Show the modal
+          const showModal = new bootstrap.Modal(
+            document.getElementById("deleteAddress")
+          );
+          showModal.show();
+        } else {
+          console.error("Error fetching user data");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    });
+  });
+  
+ 
+  function deleteAddress() {
+    const form = document.getElementById("deleteAddressData");
+    const formData = new FormData(form);
+    fetch("/checkout/delete", {
+      method: "DELETE",
+      body: JSON.stringify(Object.fromEntries(formData)),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        window.location.href = "/checkout";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  
--- server/routes/user/checkout.js
+++ server/routes/user/checkout.js
@@ -71,10 +71,11 @@ route.post('/address', logauth, async (req, res) => {
       const existdata = await checkoutCollection.findOne({ userId, _id: addressId });
 
       if (existdata) {
-        await checkoutCollection.updateOne({ _id: addressId }, { $set: req.body });
+       const editedData= await checkoutCollection.updateOne({ _id: addressId }, { $set: req.body });
+       if(editedData){
+         await checkoutCollection.findOne({ _id: addressId, userId });
 
-        const newCheckout = await checkoutCollection.findOne({ _id: addressId, userId });
-
+       }
       }
       return res.redirect("/checkout");
 
@@ -84,7 +85,9 @@ route.post('/address', logauth, async (req, res) => {
       });
 
       await newCheckout.save();
-
+      let arr=[];
+      arr.push(newCheckout._id.toString());
+      
       res.cookie("address", newCheckout._id.toString())
       
 
@@ -95,6 +98,41 @@ route.post('/address', logauth, async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+
+//delete//
+route.get("/delete/:id",logauth, async (req, res) => {
+    
+  const Idaddress = req.params.id;
+  console.log(Idaddress);
+
+  try {
+    const address = await checkoutCollection.findOne({ _id:Idaddress });
+    console.log(address);
+    if (address) {
+      res.json(address);
+    } else {
+      res.status(404).json({ error: "address not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+
+route.delete("/delete", logauth, async (req, res) => {
+  try {
+      const addrsId = req.body.id;
+
+      const deleteaddress = await checkoutCollection.findByIdAndDelete(addrsId);
+      return res.json(deleteaddress)    
+  }
+      catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+  }
+})
+
 
 
 module.exports = route;
