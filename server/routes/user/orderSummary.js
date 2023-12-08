597241c32f5c0f71889d2033222c8ebd9c72a3ce
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2023-12-08 11:02:34 +0530

    order detail page

--- server/routes/user/orderSummary.js
+++ server/routes/user/orderSummary.js
@@ -13,20 +13,44 @@ route.get('/',logauth,async(req,res) =>{
     console.log("orderdetails : ",orderDetails);
     const orderDatas = [];
     for (let orderDetail of orderDetails){
-        console.log(orderDetail.orderproducts);
         for (let product of orderDetail.orderproducts){
             const productDetails = await productCollection.findOne({ _id: product.productId});
             const total = productDetails.price * product.quantity
             const status = orderDetail.orderStatus
             const quantity = product.quantity
             
-            orderDatas.push({productDetails,total,status,quantity})
+            orderDatas.push({productDetails,total,status,quantity,orderDetail})
 
         }
     }
-
     res.render('orderSummary', {user,orderDatas,orderDetails})
 })
+
+
+route.get('/:id',logauth,async(req,res) =>{
+    const userId= req.userId;
+    const user = await userCollection.findById(userId)
+    const orderId = req.params.id;
+    const myOrder = await orderCollection.findOne({_id:orderId})
+    console.log("myorder",myOrder);
+    const addressDetails = await checkoutCollection.findOne({_id:myOrder.addressId})
+    console.log("addressDetails",addressDetails);
+    // const orderproduct = myOrder.orderproducts;
+    // console.log("orderpro",orderproduct);
+    const orderData = []
+    for (let myOrders of myOrder){
+        for (let product of myOrders.orderproducts){
+            const orderProduct = await productCollection.findOne({ _id: product.productId});
+            const quantity = product.quantity
+
+
+        }
+        orderData.push({orderProduct,quantity})
+    }
+    
+    res.render("orderDetail",{myOrder,addressDetails,user})
+})
+
 
 
 module.exports = route;
