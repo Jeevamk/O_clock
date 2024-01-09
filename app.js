d1d32aa8b4044338960f76202f5b537d892e130b
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-01-09 20:16:14 +0530

    404 page

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const cookieParser = require('cookie-parser');
 const { body , validationResult } =require ("express-validator");
 const parserencoded = bodyparser.urlencoded({ extended: false });  
 const expressHandlebars = require('express-handlebars');
-                                                                                                                  
+
+const createError = require("http-errors")
 
 
 const app = express();
@@ -68,7 +69,23 @@ app.use('/about',require('./server/routes/user/about'))
 
 
 
-
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  
+  
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('404');
+  });
 
 
 
